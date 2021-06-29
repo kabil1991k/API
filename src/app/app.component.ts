6aa24d285b1e67adc14800f1e656d5954a97427f
kabil <kabil@ancbusiness.com>
2021-06-29 16:47:52 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { RestService } from './rest.service';
+import { Users } from './Users';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Jason';
+  constructor( private ps:RestService)
+  {}
+colums = ["User id","First name","Last name","Email","Mobile","Salary"];
+index = ["id","firstname", "lastname","email","mobile","salary"];
+users : Users[]=[];
+ngOnInit():void
+{
+  this.ps.getusers().subscribe(
+    (response)=>{
+      this.users=response;
+    },
+    (error)=>{
+      console.log("error occured:"+error);
+    }
+  )
+}
+  
 }
